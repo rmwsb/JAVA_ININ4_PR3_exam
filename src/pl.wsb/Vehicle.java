b7b258bbb0ba2cfcc9ea8ce1339666da1e6a3927
rmwsb <79797498+rmwsb@users.noreply.github.com>
2021-06-26 17:19:53 +0200

    Create Vehicle.java

---
+++ src/pl.wsb/Vehicle.java
@@ -0,0 +1,10 @@
+package pl.wsb;
+
+public abstract class Vehicle {
+    String license_plate_number;
+    String vin;
+    String color;
+    float price;
+    float fuel_per_100km;
+    float gas_tank_fuel;
+}
